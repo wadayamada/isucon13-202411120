8f3737dac75dfe88418d19c507886e8b1380f2f3
shinichiro-yoshida <shinichiro.yoshida@linecorp.com>
2024-11-23 15:45:42 +0900

    Fix query of moderateHandler

--- go/livecomment_handler.go
+++ go/livecomment_handler.go
@@ -390,7 +390,7 @@ func moderateHandler(c echo.Context) error {
 	for _, ngword := range ngwords {
 		// ライブコメント一覧取得
 		var livecomments []*LivecommentModel
-		if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments"); err != nil {
+		if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments WHERE livestream_id = ?", livestreamID); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livecomments: "+err.Error())
 		}
 
@@ -399,7 +399,6 @@ func moderateHandler(c echo.Context) error {
 			DELETE FROM livecomments
 			WHERE
 			id = ? AND
-			livestream_id = ? AND
 			(SELECT COUNT(*)
 			FROM
 			(SELECT ? AS text) AS texts
@@ -407,7 +406,7 @@ func moderateHandler(c echo.Context) error {
 			(SELECT CONCAT('%', ?, '%')	AS pattern) AS patterns
 			ON texts.text LIKE patterns.pattern) >= 1;
 			`
-			if _, err := tx.ExecContext(ctx, query, livecomment.ID, livestreamID, livecomment.Comment, ngword.Word); err != nil {
+			if _, err := tx.ExecContext(ctx, query, livecomment.ID, livecomment.Comment, ngword.Word); err != nil {
 				return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
 			}
 		}
